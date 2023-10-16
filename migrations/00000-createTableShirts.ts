4751ab0a554c011ed184a377fa0ece3955df1a6c
Marko Pavleka <markopavleka@gmx.at>
2023-10-16 15:46:46 +0200

    Fix migration

--- migrations/00000-createTableShirts.ts
+++ migrations/00000-createTableShirts.ts
@@ -13,7 +13,7 @@ export async function up(sql: Sql) {
      id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
      name VARCHAR(30) NOT NULL,
      description VARCHAR(30) NOT NULL,
-     price NUMERIC(30) NOT NULL,
+     price NUMERIC(30) NOT NULL
      );
   `;
 }
